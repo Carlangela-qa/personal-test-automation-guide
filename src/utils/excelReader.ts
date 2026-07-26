1362eb24b0e05b38cee459e3271fbb274d1d35da
carla-aptx <carla@aptx.ca>
2026-07-27 00:32:09 +0800

    Comment out LoginData type definition and update readExcel function return type to any[]

--- src/utils/excelReader.ts
+++ src/utils/excelReader.ts
@@ -3,15 +3,15 @@ import path from 'path';
 
 //the structure of the data we expect from the Excel sheet.
 // This will help in ensuring that the data read from the Excel file matches our expectations and can be used effectively in our tests.
-export type LoginData = {
-    email: string;
-    password: string;
-    expected: string;
-    run: string;
-}
+// export type LoginData = {
+//     email: string;
+//     password: string;
+//     expected: string;
+//     run: string;
+// }
 
 // Function to read data from an Excel file and return it as an array of LoginData objects.
-export function readExcel(filePath: string, sheetName: string): LoginData[]{
+export function readExcel(filePath: string, sheetName: string): any[]{
 
     const fullPath = path.resolve(filePath);
     console.log('Full Path is ', fullPath);
